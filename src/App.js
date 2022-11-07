d2e47a7bbd5eae9311ed42bb7fbd22398b913f8b
Mohamed Ahmed <moho472@gmail.com>
2022-11-07 13:57:28 -0500

    updated react files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Protect from 'react-app-protect'
+import 'react-app-protect/dist/index.css'
 
 function App() {
   return (
+    <Protect
+    sha512 = 'a1b895e0fd4a8be005982e5901bb7cf880b173571bd8792365cac3177fc9c1e7d67cfb26492b66dd9e4697b35cba45e20ff92616cc14c6895508cada5802d332'
+    blur={false}
+    >
+      <div>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +26,8 @@ function App() {
         </a>
       </header>
     </div>
+    </div>
+    </Protect>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Protect from "react-app-protect"
+import "react-app-protect/dist/index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
